9619517969ee464b9955f088e1f23bc028a7ee87
houmreu <63812471+houmreu@users.noreply.github.com>
2023-05-21 08:31:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,35 @@
-const daysEl = document.getElementById("days")
-const hoursEl = document.getElementById("hours")
-const minutesEl = document.getElementById("minutes")
-const secondsEl = document.getElementById("seconds")
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const mainTextI = document.getElementById("mainTextInput");
+const mainText = document.getElementById("mainText");
 
-let dateI = "07 12 2023"
-function countdown(){
-    const useDate = new Date(`${dateI}`)
-    const currentDate = new Date()
-    const totalseconds = (useDate - currentDate) / 1000
-    const days = Math.floor(totalseconds / 3600 / 24)
-    const hours = Math.floor(totalseconds / 3600) % 24
-    const minutes = Math.floor(totalseconds / 60) % 60
-    const seconds = Math.floor(totalseconds) % 60
+let td = Number(String(new Date().getDate()).padStart(2, '0'));
+let tm = Number(String(new Date().getMonth() + 1).padStart(2, '0'));
+let ty = new Date().getFullYear();
+
+let dateI = (tm != 7 && tm < 12) ? `${7} ${12} ${ty}` : `${7} ${12} ${ty + 1}`;
+
+setInterval(() => {
+    const totalseconds = (new Date(`${dateI}`) - new Date()) / 1000;
+    const days = Math.floor(totalseconds / 3600 / 24);
+    const hours = Math.floor(totalseconds / 3600) % 24;
+    const minutes = Math.floor(totalseconds / 60) % 60;
+    const seconds = Math.floor(totalseconds) % 60;
     if(totalseconds > 0){
         daysEl.innerHTML = formatTime(days)
-        hoursEl.innerHTML = formatTime(hours)
-        minutesEl.innerHTML = formatTime(minutes)
-        secondsEl.innerHTML = formatTime(seconds)
+        hoursEl.innerHTML = formatTime(hours);
+        minutesEl.innerHTML = formatTime(minutes);
+        secondsEl.innerHTML = formatTime(seconds);
     }
-}
+}, 1000)
 
-function formatTime(time){
+const formatTime = (time) => {
     return time < 10 ? `0${time}` : time;
 }
 
-function change(){
-    changeDate()
-    changeText()
+const change = () => {
+    dateI = document.getElementById("dateInput").value != "" ? document.getElementById("dateInput").value : dateI;
+    mainText.innerHTML = mainTextI.value != "" ? mainTextI.value : mainText.innerHTML;
 }
-
-function changeText(){
-    const mainTextI = document.getElementById("mainTextInput")
-    const mainText = document.getElementById("mainText")
-    mainText.innerHTML = mainTextI.value
-}
-
-function changeDate(){
-    dateI = document.getElementById("dateInput").value
-}
-
-countdown()
-setInterval(countdown, 1000)
